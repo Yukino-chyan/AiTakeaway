e752790c03638346ea8fc2b23ed577d72f207646
You Haochen <957943776@qq.com>
2026-04-12 22:04:23 +0800

    Initial commit

---
+++ src/main/java/com/aitakeaway/server/AiTakeawayApplication.java
@@ -0,0 +1,13 @@
+package com.aitakeaway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTakeawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTakeawayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aitakeaway/server/AiTakeawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitakeaway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTakeawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
